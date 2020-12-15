c71c1ecaa88e54e843a0ab0c522d3535b6ec0774
zhourusheng <zhourusheng@yunzhangfang.com>
2020-12-15 10:51:03 +0800

    获取到地区

--- cheerio.js
+++ cheerio.js
@@ -5,13 +5,14 @@ const fs = require('fs')
 const charset = require("superagent-charset");
 const agent = require("superagent");
 charset(agent)
+// agent.buffer[mime] = true
 
 
 const url = 'http://www.dzkbw.com/city/'
 
-function getHtml(url) {
+const getHtml = async (url) => {
 
-  agent.get(url).charset('gbk').end((err, res) => {
+  await agent.get(url).charset('gbk').end((err, res) => {
     const provinceList = []
     html = res.text;
     const $ = cheerio.load(html, { decodeEntities: false })
@@ -40,15 +41,16 @@ function getHtml(url) {
     })
 
     provinceList.slice(0, 2).forEach((item, index) => {
-      // provinceList.forEach(item => {
-      item.city.forEach((city, i) => {
+      // provinceList.forEach((item, index) => {
+      item.city.forEach(async (city, i) => {
         const _url = encodeURI(`${url}${city.cityFullName}`)
-        agent.get(_url).charset('gbk').end((err, res) => {
+        await agent.get(_url).charset('gbk').end((err, res) => {
           html = res.text;
           const $ = cheerio.load(html, { decodeEntities: false })
           const $district = '#main .Districtlist ul li a'
-          $($district).map((i, val) => {
+          $($district).map((disItem, val) => {
             const text = $(val).text()
+            console.log(_url, text)
             provinceList[index].city[i].districtList.push({
               districtName: text
             })
@@ -57,10 +59,12 @@ function getHtml(url) {
       })
     })
 
-    // 写入文件
-    writerStream = fs.createWriteStream('province.json');
-    writerStream.write(JSON.stringify(provinceList, undefined, 2), 'UTF8');
-    writerStream.end();
+    setTimeout(() => {
+      // // 写入文件
+      writerStream = fs.createWriteStream('province.json');
+      writerStream.write(JSON.stringify(provinceList, undefined, 2), 'UTF8');
+      writerStream.end();
+    }, 5000);
   })
 }
 
