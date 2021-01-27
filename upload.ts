7fd8d2fccbc633bb3656e26151e1525e8d69e248
zhourusheng <zhourusheng@yunzhangfang.com>
2021-01-27 10:39:17 +0800

    调整

--- upload.ts
+++ upload.ts
@@ -11,62 +11,6 @@ const submitBtn = '.btn-primary'
 const MiddleId = 5
 const EnglishId = 52372
 
-// 版本
-const BanbenList = [
-  {
-    text: '人教新目标Go For It!（2012）',
-    value: '52373'
-  },
-  {
-    text: '人教版（五四学制）（2012）',
-    value: '52616'
-  },
-  {
-    text: '外研版（2012）',
-    value: '52686'
-  },
-  {
-    text: '仁爱版（2012）',
-    value: '53051'
-  },
-  {
-    text: '牛津译林版（2012）',
-    value: '53174'
-  },
-  {
-    text: '牛津上海版（2008）',
-    value: '53305'
-  },
-  {
-    text: '牛津深圳版（广州沈阳通用）',
-    value: '53438'
-  },
-  {
-    text: '冀教版（2012）',
-    value: '53543'
-  },
-  {
-    text: '鲁教版（五四学制）（2012）',
-    value: '53890'
-  },
-  {
-    text: '北师大版（同北京课改版）（2013）',
-    value: '53967'
-  },
-  {
-    text: '教科版（五四学制）',
-    value: '54249'
-  },
-  {
-    text: '上外版新世纪英语（五四学制）',
-    value: '54352'
-  },
-  {
-    text: '剑桥英语青少版（第二版）',
-    value: '54499'
-  }
-]
-
 const module1Url =
   'https://kfb.xbxxhz.com/dashboard/xuekewang_exercises/52894/edit'
 
@@ -138,4 +82,9 @@ const module1Url =
    */
 
   await page.goto(module1Url)
+
+  /***
+   * 内容描述搜索接口
+   * https://kfb.xbxxhz.com/dashboard/xuekewang_exercises/videos?q=try%E7%9A%84%E7%94%A8%E6%B3%95
+   */
 })()
